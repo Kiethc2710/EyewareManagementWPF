0886bab518adde2df4db3e12671a85638ec78789
HazzelNae <namdhse173232@fpt.edu.vn>
2026-03-26 09:48:22 +0700

    add account repo and staff service

---
+++ EyewearManagementSystem/BAL/Staff_Service.cs
@@ -0,0 +1,29 @@
+﻿@ -0,0 + 1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace BLL
+{
+    public class Staff_Service
+    {
+        private readonly DAL.AccountRepo _accountRepo;
+
+        public Staff_Service()
+        {
+            _accountRepo = new DAL.AccountRepo();
+        }
+
+        public Account? Authentication(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            return _accountRepo.GetByUserNameAndPassword(username, password);
+        }
+    }
+}
---
+++ EyewearManagementSystem/DAL/AccountRepo.cs
@@ -0,0 +1,19 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace DAL
+{
+    public class AccountRepo
+    {
+        public Account? GetByUserNameAndPassword(string username, string password)
+        {
+            using var db = new EyewareManagementContext();
+            return db.Accounts.FirstOrDefault(a => a.Username == username && a.Password == password);
+        }
+    }
+}
